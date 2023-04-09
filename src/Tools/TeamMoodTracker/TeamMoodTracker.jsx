3452245abcce7efd2bab3a979eb44593873484f0
bpeddapudi <82531209+bpeddapudi@users.noreply.github.com>
2023-04-09 00:07:53 -0700

    Routes

--- src/Tools/TeamMoodTracker/TeamMoodTracker.jsx
+++ src/Tools/TeamMoodTracker/TeamMoodTracker.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import { Grid, Paper, Button, TextField } from "@mui/material";
+import { 
+  useParams, Route, Routes, BrowserRouter, Link
+} from "react-router-dom";
 import './TeamMoodTracker.css';
 import { emotions } from './Emotions.js';
 
+
+// Begin mood check in
+function BeginMoodCheckIn(props) {
+  const uniqueNumber = Date.now().toString(36) + Math.random().toString(36).substring(2, 7);
+  return (
+    <Grid className="begin-test-container" container direction="row" justifyContent="center" alignItems="center">
+      <Grid item xs={4}>
+        <Paper className='beging-test-paper'>
+          <p className="tyrnow-subhead">How is your team feeling</p>
+          <h2 className="tyrnow-heading">Team Mood Check-Ins</h2>
+          <TextField className="tyrnow-team-name" label="Enter your team name" variant="outlined" />
+          <Button variant="contained" size="large" component={Link} to={`./${uniqueNumber}`} >Try it for Free</Button>
+        </Paper>
+      </Grid>
+    </Grid>
+  )
+}
+
+
+// Returns mood card based on emotion
 function MoodCard(props) {
   const getBackgroundStyle = (emotion) => {
     const primaryBgColor = emotion["backgroundColor"][0] ? emotion["backgroundColor"][0] : '#000000';
@@ -26,28 +49,32 @@ function MoodCard(props) {
   )
 }
 
-export default function TeamMoodTracker() {
+
+// Mood test component
+function MoodTest() {
+  let { testId } = useParams();
   return (
     <>
-      {/* Moode test begin */}
-      <Grid className="begin-test-container" container direction="row" justifyContent="center" alignItems="center">
-        <Grid item xs={4}>
-          <Paper className='beging-test-paper'>
-            <p className="tyrnow-subhead">How is your team feeling</p>
-            <h2 className="tyrnow-heading">Team Mood Check-Ins</h2>
-            <TextField className="tyrnow-team-name" label="Enter your team name" variant="outlined" />
-            <Button variant="contained" >Try it for Free</Button>
-          </Paper>
-        </Grid>
-      </Grid>
-
-
-      {/* Moode test */}
-      {/* <Grid container spacing={2}>
+      <p>testid:{testId}</p>
+      <Grid className="mood-test-container" container direction="row" justifyContent="center" alignItems="center" spacing={2}>
         {emotions.map(emotion =>
           <MoodCard key={emotion.zone} emotion={emotion} />
         )}
-      </Grid> */}
+      </Grid>
     </>
+  )
+}
+
+
+// Main component - TeamMoodTracker
+export default function TeamMoodTracker() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BeginMoodCheckIn />} />
+        <Route path="/moodchecker" element={<BeginMoodCheckIn />} />
+        <Route path="/moodchecker/:testId" element={<MoodTest />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
